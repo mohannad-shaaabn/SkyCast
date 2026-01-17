cbdae014a9e41406bcbea95ccd3c555545bbb0db
mohannad-shaaban <mohannadshaaban2322005@gmail.com>
2026-01-17 05:54:23 +0200

    Add weather API integration

--- src/constant/api.jsx
+++ src/constant/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const fetchWeather = async (city) => {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=46f6ad75f40b44aeb3b53357251903&q=${city}&days=3&aqi=no&alerts=no`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=46f6ad75f40b44aeb3b53357251903&q=${city}&days=3&aqi=no&alerts=no`;
   const res = await axios.get(url);
   return res.data;
 };
